464170178be7c57123493d0ca496a79f37778301
Fatih Suzer <maclabam@gmail.com>
2021-12-04 19:13:39 +0300

    Add navigation links and delete product

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -9,9 +9,15 @@
           </h1>
           <div class="a-spacing-large"></div>
           <!-- Button -->
-          <a href="#" class="a-button-buy-again">Add a new product</a>
-          <a href="#" class="a-button-history margin-right-10">Add a new category</a>
-          <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
+          <nuxt-link to="/products" class="a-button-buy-again"
+            >Add a new product</nuxt-link
+          >
+          <nuxt-link to="/category" class="a-button-history margin-right-10"
+            >Add a new category</nuxt-link
+          >
+          <nuxt-link to="/owner" class="a-button-history margin-right-10"
+            >Add a new owner</nuxt-link
+          >
           <!-- Listing Page -->
         </div>
       </div>
@@ -19,11 +25,15 @@
     <div class="a-spacing-large"></div>
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div v-for="product in products" :key="product._id" class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+        <div
+          v-for="(product, index) in products"
+          :key="product._id"
+          class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
+        >
           <div class="history-box">
             <!-- product image -->
             <a href="#" class="a-link-normal">
-              <img :src="product.photo" class="img-fluid"/>
+              <img :src="product.photo" class="img-fluid" />
             </a>
             <!-- product title -->
             <div class="a-spacing-top-base asin-title">
@@ -34,14 +44,16 @@
             <!-- product rating -->
             <div class="a-row">
               <a href="#">
-                <i class = "fas fa-star"/>
-                <i class = "fas fa-star"/>
-                <i class = "fas fa-star"/>
-                <i class = "fas fa-star"/>
-                <i class = "fas fa-star"/>
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
               </a>
               <span class="a-letter-space"></span>
-              <span class="a-color-tertiary a-size-small asin-reviews">(1732)</span>
+              <span class="a-color-tertiary a-size-small asin-reviews"
+                >(1732)</span
+              >
             </div>
             <!-- product price -->
             <div class="a-row">
@@ -51,8 +63,19 @@
             </div>
             <!-- product buttons-->
             <div class="a-row">
-              <a href="#" class="a-button-history margin-right-10"> Update</a>
-              <a href="#" class="a-button-history margin-right-10"> Delete</a>
+              <nuxt-link
+                :to="`/products/${product._id}`"
+                class="a-button-history margin-right-10"
+              >
+                Update</nuxt-link
+              >
+              <a
+                href="#"
+                class="a-button-history margin-right-10"
+                @click="onDeleteProduct(product._id, index)"
+              >
+                Delete</a
+              >
             </div>
           </div>
         </div>
@@ -65,13 +88,27 @@
 export default {
   async asyncData({ $axios }) {
     try {
-      let response = await $axios.$get("http://localhost:3000/api/products");
+      let response = await $axios.$get('http://localhost:3000/api/products')
       return {
         products: response.products,
       }
-    } catch (err) {
+    } catch (err) {}
+  },
 
-    }
-  }
+  methods: {
+    async onDeleteProduct(id, index) {
+      try {
+        let response = await this.$axios.$delete(
+          `http://localhost:3000/api/products/${id}`
+        )
+
+        if (response.success) {
+          this.products.splice(index, 1)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
 }
 </script>
