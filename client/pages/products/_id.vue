44dc62696eeac4e5f47e36fc143b53bc1b92647f
Fatih Suzer <maclabam@gmail.com>
2021-12-05 16:22:05 +0300

    Add image and author section to product page

---
+++ client/pages/products/_id.vue
@@ -0,0 +1,85 @@
+<template>
+  <main>
+    <!-- Breadcrumbs -->
+    <div class="a-spacing-top-medium"></div>
+    <div class="container-fluid">
+      <div class="wayfinding-breadcrumbs-container">
+        <ul class="a-unordered-lists a-horizontal a-size-small">
+          <li>
+            <span class="a-list-item">
+              <a class="a-link-normal a-color-tertiary" href="#"> Category</a>
+            </span>
+          </li>
+          <li>
+            <span class="a-list-item">></span>
+          </li>
+          <li>
+            <span class="a-list-item">
+              <a class="a-link-normal a-color-tertiary" href="#"> Title</a>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="dp-container">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-4">
+            <div class="leftCol">
+              <!-- Image -->
+              <div class="imgBlock">
+                <div class="eBooksimg">
+                  <img src="/img/xcase.jpg" class="img-fluid" />
+                </div>
+              </div>
+              <!-- Follow author -->
+              <div class="authorFollow">
+                <hr class="a-divider-normal" />
+                <h1 class="authorFollowHeading">Follow the author</h1>
+                <div class="a-spacing-top-small">
+                  <div class="row">
+                    <!-- Author's img -->
+                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+                      <div class="smallAuthorImageContainer">
+                        <a href="#">
+                          <img src="/img/xcase.jpg" class="img-fluid" />
+                        </a>
+                      </div>
+                    </div>
+                    <!-- Author's name -->
+                    <div class="col-xl-4 col-lg-3 col-md-3 col-sm-3 col-3">
+                      <div class="authorNameCol">
+                        <a href="#"> Walter Isaacson</a>
+                      </div>
+                    </div>
+                    <!-- Author's follow btn -->
+                      <div class="col-xl-5 col-lg-6 col-md-6 col-sm-6 col-6"> 
+                        <div class="authorBtn mt-2">
+                          <a href="#">
+                            <span class="btnFollow">
+                              <span class="a-btn-inner">
+                                <button class="a-btn-text">
+                                  +Follow
+                                </button>
+                              </span>
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
